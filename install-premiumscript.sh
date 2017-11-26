6490d56a5e29e9535ca5a8e1bd65dafc95e4d33b
jkjknm123 <jkjknm02x@gmail.com>
2017-11-27 00:38:42 +0700

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -19,9 +19,9 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.tar.gz "https://raw.githubusercontent.com/jkjknm123/VPN-THAI/master/premium-script.tar.gz"
-tar -xvf premium-script.tar.gz
-rm -f premium-script.tar.gz
+wget -O premium-script.tar.gz "https://github.com/jkjknm123/VPN-THAI/blob/master/pre.zip"
+unzip pre.zip
+
 
 cp /usr/local/bin/premium-script /usr/local/bin/menu
 
